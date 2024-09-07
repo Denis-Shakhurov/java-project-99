901188929ba3d125acc3a6b0747e890785964bbe
DenisShakhurov <disa.sdf86@gmail.com>
2024-09-07 14:20:36 +0700

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,24 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.instancio:instancio-junit:3.3.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+// Понадобится когда мы начнем работать с аутентификацией
+    testImplementation("org.springframework.security:spring-security-test")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.test {
