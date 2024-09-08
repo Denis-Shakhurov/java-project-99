92b27c0243b0d51b27848c31c740ee50e2fe7796
DenisShakhurov <disa.sdf86@gmail.com>
2024-09-08 10:33:11 +0700

    update build.gradle.kts App.java User.java

--- app/src/main/java/io/hexlet/code/App.java
+++ app/src/main/java/io/hexlet/code/App.java
@@ -1,11 +1,18 @@
 package io.hexlet.code;
 
+import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
     }
 }
